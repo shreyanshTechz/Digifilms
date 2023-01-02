07d1c9b2b5b73183369046f0f8f17aefae8dfe42
shreyanshTechz <85357239+shreyanshTechz@users.noreply.github.com>
2023-01-02 21:12:17 +0530

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -6,33 +6,36 @@ const http = require("https");
 const { json } = require('express');
 const bodyparser = require('body-parser');
 const apikey = "7914dc87e2msh63c6ef9c498e562p12e7afjsn53bb0e865ab4";
-router.use(bodyparser.urlencoded({extended:true}));
-
-router.post('/search',(req,res)=>{
+router.use(bodyparser.urlencoded({ extended: true }));
+router.get('/try',(req,res)=>{
+    res.render('try')
+})
+router.post('/search', (req, res) => {
     console.log(req.body.title);
-
+    value = req.body.title.replace(' ', '');
+    console.log(value);
     const options = {
         "method": "GET",
         "hostname": "ott-details.p.rapidapi.com",
         "port": null,
-        "path": "/search?title="+req.body.title+"&page=1",
+        "path": "/search?title=" + value + "&page=1",
         "headers": {
             "X-RapidAPI-Key": apikey,
             "X-RapidAPI-Host": "ott-details.p.rapidapi.com",
             "useQueryString": true
         }
     };
-    
+
     const reqt = http.request(options, function (resu) {
         const chunks = [];
-    
+
         resu.on("data", function (chunk) {
             chunks.push(chunk);
         });
-    
+
         resu.on("end", function () {
             const body = Buffer.concat(chunks);
-            console.log(body.toString());
+            // console.log(body.toString());
             f = JSON.parse(body);
             res.render('blog', {
                 lists: f,
@@ -40,7 +43,7 @@ router.post('/search',(req,res)=>{
             })
         });
     });
-    
+
     reqt.end();
 });
 var f, lang;
@@ -101,6 +104,7 @@ const req = http.request(options2, function (res) {
 });
 
 req.end();
+var myblog;
 
 
 router.get('/info/:slug', (req, res) => {
@@ -108,66 +112,68 @@ router.get('/info/:slug', (req, res) => {
     myblog = f.results.filter((e) => {
         return e.title == req.params.slug
     })
-    console.log(myblog[0].imdbid);
+    // console.log(myblog[0].imdbid);
 
     const options = {
         "method": "GET",
         "hostname": "ott-details.p.rapidapi.com",
         "port": null,
-        "path": "/gettitleDetails?imdbid="+myblog[0].imdbid,
+        "path": "/gettitleDetails?imdbid=" + myblog[0].imdbid,
         "headers": {
             "X-RapidAPI-Key": apikey,
             "X-RapidAPI-Host": "ott-details.p.rapidapi.com",
             "useQueryString": true
         }
     };
-    
+
     const req1 = http.request(options, function (res1) {
         const chunks = [];
-    
+
         res1.on("data", function (chunk) {
             chunks.push(chunk);
         });
-    
         res1.on("end", function () {
-            const body = Buffer.concat(chunks);
-            console.log(body.toString());
-            const optionsi = {
-                "method": "GET",
-                "hostname": "ott-details.p.rapidapi.com",
-                "port": null,
-                "path": "/getadditionalDetails?imdbid=tt7286456",
-                "headers": {
-                    "X-RapidAPI-Key": apikey,
-                    "X-RapidAPI-Host": "ott-details.p.rapidapi.com",
-                    "useQueryString": true
-                }
-            };
-            
-            const req5 = http.request(optionsi, function (res5) {
-                const chunks = [];
-            
-                res5.on("data", function (chunk) {
-                    chunks.push(chunk);
-                });
-            
-                res5.on("end", function () {
-                    const body2 = Buffer.concat(chunks);
-                    console.log(body.toString());
 
-                    res.render('blogpost', {
-                        post: myblog[0],
-                        extra : JSON.parse(body),
-                        extra2: JSON.parse(body2)
-                    })
+            setTimeout(function () {
+                const body = Buffer.concat(chunks);
+                // console.log(body.toString());
+                const optionsi = {
+                    "method": "GET",
+                    "hostname": "ott-details.p.rapidapi.com",
+                    "port": null,
+                    "path": "/getadditionalDetails?imdbid=" + myblog[0].imdbid,
+                    "headers": {
+                        "X-RapidAPI-Key": apikey,
+                        "X-RapidAPI-Host": "ott-details.p.rapidapi.com",
+                        "useQueryString": true
+                    }
+                };
+                const req5 = http.request(optionsi, function (res5) {
+                    const chunks = [];
+    
+                    res5.on("data", function (chunk) {
+                        chunks.push(chunk);
+                    });
+    
+                    res5.on("end", function () {
+                        const body2 = Buffer.concat(chunks);
+                        console.log(JSON.parse(body2));
+    
+                        res.render('blogpost', {
+                            post: myblog[0],
+                            extra: JSON.parse(body),
+                            extra2: JSON.parse(body2)
+                        })
+                    });
                 });
-            });
-            
-            req5.end();
+    
+                req5.end();
+    
+            }, 2000);
             
         });
     });
-    
+
     req1.end();
     // console.log(myblog)
 })
